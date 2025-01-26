2747eec470dceef65b3e2c5ec522b7021a0edb4e
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2025-01-25 22:42:34 -0300

    Finalização

--- obj/Debug/net9.0/TextEditor.AssemblyInfo.cs
+++ obj/Debug/net9.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893639dec6e98453d8eaced1be01c34da4a6cf49")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
